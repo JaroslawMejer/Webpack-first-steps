0c2c3cd52a0fc7f6a9961f03881935648ac69939
Jarosław Mejer <jaroslaw.mejer@gmail.com>
2018-07-05 11:41:36 +0200

    Small correct for TodoForm.js

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,23 +1,34 @@
 import React from 'react';
 
 class TodoForm extends React.Component {
-	handleSubmit(event){
-		console.log(document.getElementById('addTask').value + ' has been added');
+	constructor() {
+		super();
+		this.state = {
+			value: ''
+		}
+		this.onChangeHandle = this.onChangeHandle.bind(this)
+		this.onSubmit = this.onSubmit.bind(this)
+	}
+	onChangeHandle(event){
+		this.setState({value: event.target.value})
+	}
+	onSubmit(event){
 		event.preventDefault();
+		this.props.add(this.state.value)
 	}
     render() {
         return (
         	<div className="formContainer">
-	            <form onSubmit={this.handleSubmit}>
+	            <form onSubmit={event => this.onSubmit(event)}>
 	            	<label>Add a task</label>
 	            	<input
 	            		type="text"
-	            		id="addTask"
+	            		id="value"
+	            		onChange={event => this.onChangeHandle(event)}
 	            		/>
 	            	<input
 	            		type="submit"
-	            		value="Submit"
-	            		onClick={e => {this.props.add(document.getElementById('addTask').value)}}/>
+	            		value="Submit" />
 	            </form>
 	        </div>
         );
